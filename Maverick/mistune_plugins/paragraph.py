cc33e3499bb7c39ae6da350f7b2bb35027c81060
AlanDecode <zytbuaa1415@gmail.com>
2020-08-11 16:26:53 +0800

    render figure directly when there are only one figure tag inside paragraph

--- Maverick/mistune_plugins/paragraph.py
+++ Maverick/mistune_plugins/paragraph.py
@@ -3,9 +3,15 @@ import mistune
 
 class ParagraphRenderer(mistune.HTMLRenderer):
     def paragraph(self, text):
+        # text contains figure tag
         if text.strip().startswith('<figure'):
-            text = [item.strip() for item in text.strip().split('\n')]
-            text = ['<div class="photos">%s</div>' % item for item in text]
-            return '<div class="photoset">%s</div>' % ''.join(text)
-        else:
-            return '<p>%s</p>' % text
+            text = text.strip()
+            if text[1:].find('<figure') > -1:
+                # if there are at lease 2 figure tags
+                text = [item.strip() for item in text.split('\n')]
+                text = ['<div class="photos">%s</div>' % item for item in text]
+                return '<div class="photoset">%s</div>' % ''.join(text)
+            else:
+                return text
+
+        return '<p>%s</p>' % text
